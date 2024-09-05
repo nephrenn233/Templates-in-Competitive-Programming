691f52dca26ab145f380d430c28f6d0d71b6fe52
nephrenn233 <nephrenn233@outlook.com>
2024-09-05 18:27:09 +0800

    add 并查集

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # Templates-in-Competitive-Programming
+
 自用算法竞赛模板，来源于我自己和网络。
 
 ## Done
+
 目前已经完成的模板
 
 ### 数据结构 data structure
+
 树状数组，支持单点修改、前缀和查询、对于权值树状数组查询第 $k$ 小数（Fenwick Tree.cpp）
+并查集，支持查询集合、查询集合大小、合并
 
 ### 杂项 various
+
 取模类（ModInt.cpp）
---
+++ data structure/Disjoint Set Union.cpp
@@ -0,0 +1,40 @@
+class DSU {
+// 1-indexed
+    std::vector<int> f, sz;
+
+    DSU () {}
+    DSU (int _n) {
+        init(_n);
+    }
+
+    void init (int _n) {
+        f.resize(_n + 1);
+        std::iota(f.begin(), f.end(), 0);
+        sz.assign(_n + 1, 1);
+        sz[0] = 0;
+    }
+
+    int find (int x) {
+        while (x != f[x]) {
+            x = f[x] = f[f[x]];
+        }
+        return x;
+    }
+
+    bool same (int x, int y) {
+        return find(x) == find(y);
+    }
+
+    bool merge (int x, int y) {
+        // merge y to x
+        x = find(x), y = find(y);
+        if (x == y) return false;
+        sz[x] += sz[y];
+        f[y] = x;
+        return true;
+    }
+    
+    int size (int x) {
+        return sz[find(x)];
+    }
+};
