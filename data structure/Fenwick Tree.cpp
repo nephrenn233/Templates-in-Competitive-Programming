4fd5cd450dfe1f3071037be6dc3fdd7d6645caae
nephrenn233 <nephrenn233@outlook.com>
2024-09-06 21:11:54 +0800

    fix: 树状数组

--- data structure/Fenwick Tree.cpp
+++ data structure/Fenwick Tree.cpp
@@ -1,5 +1,6 @@
 template <typename T>
 struct Fenwick {
+// 1-indexed
 private:
     int n;
     std::vector<T> tr;
@@ -9,29 +10,19 @@ public:
     }
     void init (int _n) {
         n = _n;
-        tr.assign(n, T{});
+        tr.resize(n + 1);
+        tr.assign(n + 1, T(0));
     }
     void add (int p, const T &v) {
-        for (int i = p + 1; i <= n; i += i & -i) {
-            tr[i - 1] += v;
+        for (int i = p; i <= n; i += i & -i) {
+            tr[i] += v;
         }
     }
     T sum (int p) {
-        T res {};
-        for (int i = p; i > 0; i -= i & -i) {
-            res += tr[i - 1];
+        T res (0);
+        for (int i = p; i >= 1; i -= i & -i) {
+            res += tr[i];
         }
         return res;
-    }
-    int kth (const T &k) {
-        int x = 0;
-        T cur {};
-        for (int i = 1 << std::__lg(n); i; i /= 2) {
-            if (x + i <= n && cur + tr[x + i - 1] <= k) {
-                x += i;
-                cur += tr[x - 1];
-            }
-        }
-        return x;
     }
 };
