ab0ddea233943945df77d877f119b02e8a2bf2a7
nephrenn233 <nephrenn233@outlook.com>
2024-12-05 18:02:56 +0800

    update: 1-n inverse

--- math/1-n-inverse.cpp
+++ math/1-n-inverse.cpp
@@ -1,6 +1,8 @@
-const int N = 1e5, P = 998244353;
-int inv[N + 10];
-inv[1] = 1;
-for (int i = 2; i <= n; ++i) {
-    inv[i] = (P - P / i) * inv[P % i] % P;
+std::vector<i64> inverse(int n, const i64 &p) {
+    std::vector<i64> inv(n + 1, 0);
+    inv[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        inv[i] = (p - p / i) * inv[p % i] % p;
+    }
+    return inv;
 }
